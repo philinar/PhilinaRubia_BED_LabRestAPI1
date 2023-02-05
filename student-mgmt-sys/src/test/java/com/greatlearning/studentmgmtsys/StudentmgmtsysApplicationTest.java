c7eee211c29cf4e18cd02420d3234b55d57b8900
philinar <113802131+philinar@users.noreply.github.com>
2023-02-06 02:26:09 +0530

    Add files via upload

---
+++ student-mgmt-sys/src/test/java/com/greatlearning/studentmgmtsys/StudentmgmtsysApplicationTest.java
@@ -0,0 +1,13 @@
+package com.greatlearning.studentmgmtsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMgmtSysApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
